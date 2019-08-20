e61806b2ad8f52f00fa1d9895d2f7ee5cbb45676
qiufeihong <qiufeihong2018@126.com>
2019-08-20 16:15:19 +0800

    fix: space

--- routers/superAdminOperation.js
+++ routers/superAdminOperation.js
@@ -153,26 +153,26 @@ router.post('/:id', function(req, res) {
 
 router.delete('/:id', function(req, res) {
   SuperAdminMap.findByIdAndRemove({
-      _id: req.params.id
-    }).then((doc) => {
-      if (doc) {
-        log.info(`Delete ${req.params.id} success`);
-        res.status(200).json({
-          state: 'ok',
-          message: `Delete ${req.params.id} success`
-        });
-      } else {
-        log.error(`Delete ${req.params.id} error`);
-        res.status(200).json({
-          state: 'err',
-          message: `Delete ${req.params.id} error`
-        });
-      }
-    }, (err) => {
-      const ERROR = new Error(err);
-      ERROR.type = COMMON.DATABASE_FAILUER;
-      throw ERROR;
-    })
+    _id: req.params.id
+  }).then((doc) => {
+    if (doc) {
+      log.info(`Delete ${req.params.id} success`);
+      res.status(200).json({
+        state: 'ok',
+        message: `Delete ${req.params.id} success`
+      });
+    } else {
+      log.error(`Delete ${req.params.id} error`);
+      res.status(200).json({
+        state: 'err',
+        message: `Delete ${req.params.id} error`
+      });
+    }
+  }, (err) => {
+    const ERROR = new Error(err);
+    ERROR.type = COMMON.DATABASE_FAILUER;
+    throw ERROR;
+  })
     .catch((err) => {
       log.error(err.message);
       res.status(500).json({
@@ -257,33 +257,33 @@ router.put('/:id', function(req, res) {
     log.error(err.message);
   });
   SuperAdminMap.findByIdAndUpdate({
-      _id: req.params.id
-    }, {
-      $set: newWebsite
-    }, {
-      new: true,
-      upsert: true,
-      setDefaultsOnInsert: true,
-      setOnInsert: true
-    }).then((doc) => {
-      if (doc) {
-        log.info(`Update ${req.params.id} success`);
-        res.status(200).json({
-          state: 'ok',
-          message: `Update ${req.params.id} success`
-        });
-      } else {
-        log.error(`Update ${req.params.id} error`);
-        res.status(200).json({
-          state: 'err',
-          message: `Update ${req.params.id} error`
-        });
-      }
-    }, (err) => {
-      const ERROR = new Error(err);
-      ERROR.type = COMMON.DATABASE_FAILUER;
-      throw ERROR;
-    })
+    _id: req.params.id
+  }, {
+    $set: newWebsite
+  }, {
+    new: true,
+    upsert: true,
+    setDefaultsOnInsert: true,
+    setOnInsert: true
+  }).then((doc) => {
+    if (doc) {
+      log.info(`Update ${req.params.id} success`);
+      res.status(200).json({
+        state: 'ok',
+        message: `Update ${req.params.id} success`
+      });
+    } else {
+      log.error(`Update ${req.params.id} error`);
+      res.status(200).json({
+        state: 'err',
+        message: `Update ${req.params.id} error`
+      });
+    }
+  }, (err) => {
+    const ERROR = new Error(err);
+    ERROR.type = COMMON.DATABASE_FAILUER;
+    throw ERROR;
+  })
     .catch((err) => {
       log.error(err.message);
       res.status(500).json({
